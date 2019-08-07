17e7fe72bfdc2bdc24f89bfd0e189ade0e8c7a91
Mario Nemuel Flores Condori <marioflorescondori@gmail.com>
2019-08-07 07:14:19 -0400

    creating Weather Component

---
+++ src/components/WeatherComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function WeatherComponent() {
+    return (
+        <div className="WeatherComponent">
+            <h1>Weather Location</h1>
+        </div>
+    );
+}
+
+export default WeatherComponent;
