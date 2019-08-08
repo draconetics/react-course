da5340b43a96a9c85893baa6f9a3c46e0912ed48
mario flores <marioflorescondori@gmail.com>
2019-08-08 18:33:43 -0400

    probando set state con el boton actualizar

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{Component} from 'react';
 import Location from './LocationComponent';
 import Data from './WeatherData';
 import {CLOUD, SUN} from '../../shared/weather';
@@ -11,14 +11,41 @@ const dataObject = {
     humidity: 15,
     wind: '10 m/s'
 }
+const dataObject02 = {
+    temperature: 30,
+    weatherState: 'CLOUD',
+    humidity: 20,
+    wind: '30 m/s'
+}
 
-function WeatherLocation() {
+class WeatherLocation extends Component{
+
+constructor(){
+    super();
+    this.state = {
+        city:'Buenos Aires',
+        data: dataObject
+    };
+}
+
+handleUpdateClick(e){
+    console.log('handle click');
+    this.setState({
+        city: 'Brazil',
+        data: dataObject02
+    });
+}
+
+render() {
     return (
         <div className="WeatherLocation">
-            <Location city="Buenos Aires" />
-            <Data data={dataObject}/>
+            <Location city={this.state.city} />
+            <Data data={this.state.data}/>
+            <button onClick={e=>this.handleUpdateClick(e)}>Update</button>
         </div>
     );
+}
+
 }
 
 export default WeatherLocation;
